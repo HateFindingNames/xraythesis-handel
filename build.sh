85d090ccd66013d83281b3007c8f7d0774bcc9ea
HateFindingNames <52651426+HateFindingNames@users.noreply.github.com>
2023-08-12 09:43:01 +0200

    docs merged

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-set -e
-TMP=/tmp sh swtoolkit/hammer.sh --verbose --samples --udxp --no-udxps --no-xw --no-serial --no-xmap --no-stj
+set -x
+#TMP=/tmp sh swtoolkit/hammer.sh --verbose --samples --udxp --no-udxps --no-xw --no-serial --no-xmap --no-stj
+#TMP=/tmp sh swtoolkit/hammer.sh --verbose --udxp --x64 2>&1 | tee ./build.log
+rm -r ./scons-out
+TMP=/tmp sh swtoolkit/hammer.sh --udxp --no-udxps --no-xw --no-serial --no-xmap --no-stj --no-mercury --no-saturn --x64 --verbose 2>&1 | tee ./build.log
