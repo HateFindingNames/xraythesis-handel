5036c55432586a12b8082b452cb0c20269203b35
HateFindingNames <52651426+HateFindingNames@users.noreply.github.com>
2023-09-13 18:49:04 +0200

    Bugfixes

--- myapp/thetube.py
+++ myapp/thetube.py
@@ -1,4 +1,5 @@
 import pigpio
+from gpiozero import LED
 
 class Tube:
     def __init__(self, spi_bus: int = 0):
@@ -6,8 +7,17 @@ class Tube:
         self.spi_bus = spi_bus
 
         self.pi = pigpio.pi()
+        self.filrdypin = 24
+        self.pi.set_pull_up_down(self.filrdypin, pigpio.PUD_DOWN)
+        self.pi.set_mode(self.filrdypin, pigpio.INPUT) # GPIO 24
 
-        self.dac = self.pi.spi_open(0, 20000000, 1) # device 0 at 20MHz using mode 1 (clk-polarity 0, clock-phase 1)
+        self.enpin = 25
+        self.pi.set_pull_up_down(self.enpin, pigpio.PUD_DOWN)
+        self.pi.set_mode(self.enpin, pigpio.OUTPUT) # GPIO 25
+        self.pi.write(self.enpin, 0)
+        self.enabled = 0
+
+        self.dac = self.pi.spi_open(1, 20000000, 1) # device 0 at 20MHz using mode 1 (clk-polarity 0, clock-phase 1)
         self.R1_msk = 0x8000 # bit 15
         self.SPD_msk = 0x4000 # 1: fast mode; 0: slow mode
         self.PWR_msk = 0x2000 # 1: power down; 0: normal operation
@@ -15,26 +25,35 @@ class Tube:
 
         # ADC SPI-Com config: T=1 (LSB first on MOSI) (3-wire mode) nnnn=0001 (write 1 bytes then read) W=1 (3-wire) mm=11 (POL1, PHA1)
         # --> 0x4403
-        self.adc = self.pi.spi_open(1, 100000, 0) # device 1 at 20MHz using mode 1 (clk-polarity 0, clock-phase 1)
-        self.adc_read = 0x19fff # Single ended, ODD, channel 1
+        self.adc = self.pi.spi_open(0, 100000, 0) # device 1 at 100kHz using mode 0 (clk-polarity 0, clock-phase 0)
+        self.adc_read = 0x1Bfff # Single ended, ODD, channel 1
 
+    def toggle(self):
+        self.enabled = not self.enabled
+        self.pi.write(self.enpin, self.enabled)
 
+    def enable(self, enable: bool = False):
+        self.enabled = enable
+        self.pi.write(self.enpin, enable)
+    
     def read(self,
-             channel: bool,
-             msbf: bool = True) -> bytearray:
+             channel: bool) -> bytearray:
         """Reads analog signals on the MCP3202s CH0 or CH1."""
+
         # preparations
-        adc_bytes = self.adc_read | (msbf << 13)
         adc_bytes = self.adc_read | (channel << 14)
-        adc_bytes = adc_bytes.to_bytes(3, "big")
+        adc_bytes = adc_bytes.to_bytes(3, "big") # 0b00000001 11011111 11111111
+
         # ready to write/read
-        res = self.pi.spi_xfer(self.adc, adc_bytes)
-        return res
+        n, b = self.pi.spi_xfer(self.adc, adc_bytes)
+        res = (b[-2] << 8) | b[-1] # remove most significant byte
+        res = res & ~(0xf000) # set bits 12-15 zero
+        return [res, 5 * res/4095]
 
     def composeBytesDac(self,
                         data: int,
-                        channel: bool = 1,
-                        speed="fast",
+                        channel: bool,
+                        fast: bool = True,
                         pwr: bool = True) -> list:
         """
         Register-Select Truth Table:
@@ -46,7 +65,7 @@ class Tube:
         1   1   Reserved
         -------------------
         """
-        channel != channel
+        channel = not channel
 
         if int(data) > 4095:
             print("Data too large. Must be 0 < data <= 4095!")
@@ -58,23 +77,13 @@ class Tube:
             return [payload[0], payload[1]]
         else:
             payload = 0x4000 # fast mode default            
-            if speed == "slow":
+            if not fast:
                 payload = 0x0000
 
         payload = payload | (channel << 15)
-            # self.set_bit(payload, self.R1_msk, 1)
-
         payload = payload | data
         payload = payload.to_bytes(2, "big")
-        return [payload[0], payload[1]]
-    
-    # def set_bit(self, bytes, bit_mask, bit) -> bytes:
-    #     data = bytes[0] << 8
-    #     data = data | bytes[1]
-    #     if bit == 1:
-    #         return data | bit_mask
-    #     if bit == 0:
-    #         return data & ~bit_mask
+        return payload
 
     def setVolt(self, volt: float, channel: bool) -> None:
         """Set Output in Volts on specified channel."""
@@ -82,7 +91,7 @@ class Tube:
             val = int(4095 * volt / 5)
             val = self.composeBytesDac(val, channel)
         else:
-            volt = self.composeBytesDac(0, channel)
+            val = self.composeBytesDac(0, channel)
             print("Value must be in range 0-5!")
 
         self.pi.spi_write(self.dac, val)
